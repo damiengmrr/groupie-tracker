43f7f5680bd7067b9bdb25d4d989810eaa32b2c7
24PADROL <padrolsensei@gmail.com>
2025-01-06 00:46:10 +0100

    Not working, test API

--- main.go
+++ main.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	g "groupie-tracker/backgo"
-	"fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-
 )
 
 func main(){
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api")
-	if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
-
-    responseData, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(string(responseData))
+    g.GetAPI()
 	g.Web()
 }
