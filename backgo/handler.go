04803aedc608dc231e3264a844eb48fb2734f47d
24PADROL <padrolsensei@gmail.com>
2025-01-17 23:43:27 +0100

    dev de la barre de recherche

--- backgo/handler.go
+++ backgo/handler.go
@@ -8,11 +8,12 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	// "strings"
 	"text/template"
 	// "time"
 )
 
-func Home(w http.ResponseWriter, r *http.Request){
+func Home(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./serv/home.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -20,22 +21,21 @@ func Home(w http.ResponseWriter, r *http.Request){
 	}
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
 
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    responseData, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
 	var GroupList list
 	json.Unmarshal(responseData, &GroupList.Lists)
-	t.Execute(w,GroupList)
+	t.Execute(w, GroupList)
 }
 
-
-func Artists(w http.ResponseWriter, r *http.Request){
+func Artists(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./serv/artists.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -44,23 +44,45 @@ func Artists(w http.ResponseWriter, r *http.Request){
 
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
 
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    responseData, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
 	var GroupList []artists
 	json.Unmarshal(responseData, &GroupList)
 
 	id, _ := strconv.Atoi(r.FormValue("id"))
-	t.Execute(w,GroupList[id-1])
+	t.Execute(w, GroupList[id-1])
 }
 
-// func Filter(){
-// 	parsedTime, _ := time.Parse(r.FormValue("dates"))
+// func SearchBar(w http.ResponseWriter, r *http.Request) {
+// 	t, err := template.ParseFiles("./serv/home.html")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
 
+// 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+// 	if err != nil {
+// 		fmt.Print(err.Error())
+// 		os.Exit(1)
+// 	}
+
+// 	responseData, err := ioutil.ReadAll(response.Body)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	var GroupList list
+// 	json.Unmarshal(responseData, &GroupList.Lists)
+	
+// 	id, _ := strconv.Atoi(r.FormValue("id"))
+// 	if strings.Contains(GroupList.Lists[id-1].Name, input) {
+// 		t.Execute(w, GroupList.Lists[id-1].Name)
+
+// 	}
 // }
