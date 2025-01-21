7a956d6cde27a08c81a84b6f59ad0f16539c9f0c
Bubuche <guilhem.mandallena@ynov.com>
2025-01-21 11:41:25 +0100

    barre de recherche

--- backgo/handler.go
+++ backgo/handler.go
@@ -6,30 +6,41 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+//	"os"
 	"text/template"
 )
 
-func Home(w http.ResponseWriter, r *http.Request){
+
+func Home(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./serv/home.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	
+	searchQuery := r.URL.Query().Get("search")
+
+	
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
+		fmt.Print(err.Error())
+		log.Fatal(err)
+	}
 
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    responseData, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
 	var GroupList List
 	json.Unmarshal(responseData, &GroupList.Lists)
-	t.Execute(w,GroupList)
+
+	
+	if searchQuery != "" {
+		GroupList.Lists = filterArtists(GroupList.Lists, searchQuery)
+	}
+
+	
+	t.Execute(w, GroupList)
 }
-
-
