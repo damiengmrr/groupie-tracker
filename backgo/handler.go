e7100ac81d9234ad8253e4583a5f49554e2f4d65
Bubuche <guilhem.mandallena@ynov.com>
2025-01-21 11:57:12 +0100

    recuperation de code

--- backgo/handler.go
+++ backgo/handler.go
@@ -6,7 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-//	"os"
+	"os"
+	"strconv"
 	"text/template"
 )
 
@@ -43,4 +44,29 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	
 	t.Execute(w, GroupList)
+}
+
+	
+func Artists(w http.ResponseWriter, r *http.Request){
+	t, err := template.ParseFiles("./serv/artists.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w,nil)
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+    }
+
+    responseData, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+	var GroupList []artists
+	json.Unmarshal(responseData, &GroupList)
+	
+	id, _ := strconv.Atoi(r.FormValue("id"))
+	t.Execute(w,GroupList[id-1])
 }
